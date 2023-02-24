9474445e5e9be3760cb18b1c5e052d68878d3c37
NFO-2 <NFO-2@NFO-2>
2023-02-24 12:26:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Check_Even_And_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to test whether it is EVEN or ODD:");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            
+
+            if (dividend % 2 == 0)
+            {
+                Console.WriteLine("The given number is EVEN");
+            }
+            else if (dividend % 2 == 1)
+            {
+                Console.WriteLine("The given number is ODD");
+            }
+            
+
+
+
+        }
+    }
+}
